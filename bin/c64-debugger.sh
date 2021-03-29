9e9493bb42dfab89ec870e380fe7743134102419
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2021-03-29 14:12:48 -0400

    Fix c64Debugger settings.

--- bin/c64-debugger.sh
+++ bin/c64-debugger.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-debuggerBin="/Applications/C64Debugger.app/Contents/MacOS/C64Debugger -pass"
+debuggerBin="/Applications/C64\ Debugger.app/Contents/MacOS/C64\ Debugger -pass"
 $debuggerBin -unpause -clearsettings
 # $debuggerBin -wait 500 -snapshot clean-c64.snap 
 $debuggerBin -wait 1000 -clearsettings -autojmp -snapshot clean-c64.snap -prg $1 -symbols $2
